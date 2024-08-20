dc3361bb1e4335f9b9d8fb81dfea8ff9960c5104
lancer1126 <lancer1582@gmail.com>
2024-08-20 22:33:49 +0800

    主页菜单搭建；设置页初步搭建

---
+++ Assets/Scripts/System/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace System
+{
+    public class Menu : MonoBehaviour
+    {
+        public GameObject settingUI;
+
+        private void Awake()
+        {
+            settingUI.SetActive(false);
+        }
+
+        public void StartGame()
+        {
+            Debug.Log("开始游戏");
+        }
+
+        public void OpenSetting()
+        {
+            Debug.Log("打开设置");
+            settingUI.SetActive(true);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("退出游戏");
+            Application.Quit();
+        }
+    }
+}
